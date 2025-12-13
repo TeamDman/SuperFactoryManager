3e8c8b65c0693d2833c6fb8d0c1ecb863d7ddfe9
Mason Dechaineux <mason@thebos.com.au>
2025-12-13 13:06:03 +1000

    add program disk duplication recipe

--- src/main/java/ca/teamdman/sfm/common/registry/SFMRecipes.java
+++ src/main/java/ca/teamdman/sfm/common/registry/SFMRecipes.java
@@ -2,6 +2,7 @@ package ca.teamdman.sfm.common.registry;
 
 import ca.teamdman.sfm.SFM;
 import ca.teamdman.sfm.common.CommonProxy;
+import ca.teamdman.sfm.common.recipe.DiskDuplicateRecipe;
 import ca.teamdman.sfm.common.recipe.DiskResetRecipe;
 import ca.teamdman.sfm.common.recipe.LabelGunResetRecipe;
 import net.minecraft.item.crafting.IRecipe;
@@ -11,10 +12,12 @@ public class SFMRecipes {
 
     public static IRecipe DISK_RESET;
     public static IRecipe LABEL_GUN_RESET;
+    public static IRecipe DISK_DUPLICATE;
 
     public static void initialize() {
         DISK_RESET = prepareRegister(new DiskResetRecipe(), "disk_reset");
         LABEL_GUN_RESET = prepareRegister(new LabelGunResetRecipe(), "label_gun_reset");
+        DISK_DUPLICATE = prepareRegister(new DiskDuplicateRecipe(), "disk_duplicate");
     }
 
     private static <T extends IRecipe> T prepareRegister(T recipe, String name) {
