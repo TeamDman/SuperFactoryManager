578cc3bdf604e0a386305474ba961d36bbecea74
TeamDman <TeamDman9201@gmail.com>
2026-03-08 20:03:37 -0400

    1.20(mod) - fix duplicate methods from merging (again)

--- platform/minecraft/src/main/java/ca/teamdman/sfm/common/command/SFMCommand.java
+++ platform/minecraft/src/main/java/ca/teamdman/sfm/common/command/SFMCommand.java
@@ -193,15 +193,6 @@ public class SFMCommand {
         commandSourceStack.sendSuccess(componentSupplier, true);
     }
 
-    @MCVersionDependentBehaviour
-    private static void sendSuccess(
-            CommandSourceStack commandSourceStack,
-            Supplier<Component> componentSupplier
-    ) {
-
-        commandSourceStack.sendSuccess(componentSupplier.get(), true);
-    }
-
     private static int giveKitToPlayers(
             CommandSourceStack source,
             Collection<ServerPlayer> targets
