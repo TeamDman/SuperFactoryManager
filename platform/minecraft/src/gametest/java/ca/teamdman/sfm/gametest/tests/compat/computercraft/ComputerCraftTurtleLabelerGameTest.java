58c0411c3e476372123c5c33e1caa3863a123645
TeamDman <TeamDman9201@gmail.com>
2026-07-15 23:40:54 -0400

    refactor: bound turtle upgrade lookups

--- platform/minecraft/src/gametest/java/ca/teamdman/sfm/gametest/tests/compat/computercraft/ComputerCraftTurtleLabelerGameTest.java
+++ platform/minecraft/src/gametest/java/ca/teamdman/sfm/gametest/tests/compat/computercraft/ComputerCraftTurtleLabelerGameTest.java
@@ -68,7 +68,7 @@ public class ComputerCraftTurtleLabelerGameTest extends SFMGameTestDefinition {
         ItemStack nonBlankGun = new ItemStack(SFMItems.LABEL_GUN.get());
         LabelGunItem.setActiveLabel(nonBlankGun, "non_blank");
         helper.assertTrue(
-                TurtleUpgrades.instance().get(nonBlankGun) == null,
+                !hasTurtleUpgrade(nonBlankGun),
                 "A label gun carrying state was incorrectly accepted for turtle equip"
         );
 
@@ -161,6 +161,11 @@ public class ComputerCraftTurtleLabelerGameTest extends SFMGameTestDefinition {
     @MCVersionDependentBehaviour
     private static ITurtleUpgrade findTurtleUpgrade(ItemStack itemStack) {
         return TurtleUpgrades.instance().get(itemStack);
+    }
+
+    @MCVersionDependentBehaviour
+    private static boolean hasTurtleUpgrade(ItemStack itemStack) {
+        return TurtleUpgrades.instance().get(itemStack) != null;
     }
 
 }
