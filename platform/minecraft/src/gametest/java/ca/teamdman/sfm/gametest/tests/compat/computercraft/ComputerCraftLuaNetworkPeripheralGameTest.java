f64a9df4a0a436030133f43a2c9e51fc571d9b0d
TeamDman <TeamDman9201@gmail.com>
2026-07-16 21:05:32 -0400

    fix Java source audit parse failures

--- platform/minecraft/src/gametest/java/ca/teamdman/sfm/gametest/tests/compat/computercraft/ComputerCraftLuaNetworkPeripheralGameTest.java
+++ platform/minecraft/src/gametest/java/ca/teamdman/sfm/gametest/tests/compat/computercraft/ComputerCraftLuaNetworkPeripheralGameTest.java
@@ -145,7 +145,6 @@ public class ComputerCraftLuaNetworkPeripheralGameTest extends SFMGameTestDefini
             );
             helper.succeed();
         });
-        });
     }
 
     static String terminalContents(ServerComputer computer) {
