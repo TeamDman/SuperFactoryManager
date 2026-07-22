8b951a76cd8dea60715861a3789d46a0625500c9
TeamDman <TeamDman9201@gmail.com>
2026-07-22 17:07:10 -0400

    Add repository review bundle producer

--- platform/cli/sfm-propagate-changes/src/lib.rs
+++ platform/cli/sfm-propagate-changes/src/lib.rs
@@ -15,6 +15,7 @@ pub mod paths;
 pub(crate) mod payload_fetcher;
 pub mod prism;
 pub mod propagate;
+pub mod repository_review_bundle_v1;
 pub mod review_session_v1;
 pub mod sfm_path;
 pub mod source_archive;
