41fa308d740106360e564c158df63aa0b10d4d82
TeamDman <TeamDman9201@gmail.com>
2026-08-06 23:16:39 -0400

    Refresh workspace navigation grammar test

--- platform/minecraft/src/test/java/ca/teamdman/sfm/client/action/SFMWorkspaceNavigationActionGrammarTests.java
+++ platform/minecraft/src/test/java/ca/teamdman/sfm/client/action/SFMWorkspaceNavigationActionGrammarTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class FocusPanelActionTests {
+class SFMWorkspaceNavigationActionGrammarTests {
     @Test
     void indexedFocusIsARealHierarchicalActionWithBoundedArgument() {
         ResourceLocation actionId = new ResourceLocation("sfm", "panel/focus/index");
